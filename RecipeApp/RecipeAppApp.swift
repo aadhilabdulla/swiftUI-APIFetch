ce47e47765db606498c2cc1ff0edcb7a16fbfa17
Aadhil Abdulla <aadhil242@gmail.com>
2025-11-05 09:32:49 +0530

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Aadhil Abdulla on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
